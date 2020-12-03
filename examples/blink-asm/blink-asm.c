d9803aef4feda25b80e488e5348718ec3a470037
Anderson Costa <arcostasi@gmail.com>
2020-12-03 18:48:00 -0300

    Updated examples

--- examples/blink-asm/blink-asm.c
+++ examples/blink-asm/blink-asm.c
@@ -1,12 +1,6 @@
 /*
  * Blink an LED "L" every 250 ms, using assembly routines.
  *
- * The hex file is already in the folder,
- * just click on run to simulate.
- *
- * If you want to compile the code, use your arduino platform,
- * the server does not support assembly files.
- *
  * For more details see in:
  * http://forum.arduino.cc/index.php?topic=159572#msg1194604
  */
